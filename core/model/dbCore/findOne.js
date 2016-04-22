5bed2d1b92a30b64d23a8f64582b57194bc3c7c3
Utkarsh85 <utkarshsharma8519@gmail.com>
2016-04-22 16:02:12 +0530

    Resolution for undefined query parameter

--- core/model/dbCore/findOne.js
+++ core/model/dbCore/findOne.js
@@ -7,6 +7,8 @@ module.exports= function (model,query) {
 	var db = require( '../../db/mongodb/mongodb' ).getDb();
 	
 	return new Promise(function (resolve,reject) {
+		if(!query)
+			query={};
 		queryObj= queryUtil.id_obj(query);
 		db.collection( model ).findOne(queryObj,function (err,docs) {
 			if(err)
