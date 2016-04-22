a9f82f71fcfbed937360f0220eced33f4ea47973
Utkarsh85 <utkarshsharma8519@gmail.com>
2016-04-22 17:14:01 +0530

    Added ObjectID support for native

--- core/model/model.js
+++ core/model/model.js
@@ -1,5 +1,5 @@
 var buildModel= require('./build/build');
-
+var ObjectID = require('mongodb').ObjectID;
 module.exports= function () {
 	var models = require('../utils/getModels');
 
@@ -10,5 +10,13 @@ module.exports= function () {
 		ReturnFullModel[modelName] = buildModel(modelName,models[modelName]);
 	}
 
+	ReturnFullModel.id2bson = function (id) {
+			return new ObjectID(id);
+	},
+
+	ReturnFullModel.generateId = function () {
+		return new ObjectID();
+	}
+	
 	return ReturnFullModel;
 }
